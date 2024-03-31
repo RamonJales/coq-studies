c4094d324e72ec8975711e69d1636ab53f61bb9f
RamonJales <ramonjales123@gmail.com>
2024-03-31 00:04:56 -0300

    alternate function in list

--- list.v
+++ list.v
@@ -87,4 +87,26 @@ Example test_nonzeros:
   nonzeros [0;1;0;2;3;0;0] = [1;2;3].
 Proof. reflexivity. Qed.
 
+Fixpoint alternate (l1 l2 : natlist) : natlist :=
+  match l1 with
+  |nil => l2
+  |x :: xs => match l2 with
+              |nil => x::xs
+              |y::ys => x :: y ::
+                  (alternate xs ys)
+              end
+  end.
 
+Example test_alternate1:
+  alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
+  Proof. reflexivity. Qed.
+Example test_alternate2:
+  alternate [1] [4;5;6] = [1;4;5;6].
+  Proof. reflexivity. Qed.
+Example test_alternate3:
+  alternate [1;2;3] [4] = [1;4;2;3].
+  Proof. reflexivity. Qed.
+Example test_alternate4:
+  alternate [] [20;30] = [20;30].
+  Proof. reflexivity. Qed.
+Compute alternate ([1;2]) ([3;4]).
